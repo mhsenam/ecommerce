ad45b8dc66c8166dcce556e123979888e795af76
mhsenam <mohsenamini1081@gmail.com>
2023-12-09 18:58:44 +0330

    added footer icons and copyright

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -2,17 +2,22 @@
 
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
-import Icon from '../../../../payload/fields/richText/label/Icon/index'
-import { inclusions, noHeaderFooterUrls } from '../../../constants'
+import link from '../../../../payload/fields/link'
+import { Footer, Media } from '../../../../payload/payload-types'
+import { inclusions, noHeaderFooterUrls, profileNavItems } from '../../../constants'
+import { Button } from '../../Button'
 import { Gutter } from '../../Gutter'
+import { Label } from '../../Label/index'
 
 import classes from './index.module.scss'
 
-const FooterComponent = () => {
+const FooterComponent = ({ footer }: { footer: Footer }) => {
   const pathname = usePathname()
 
+  const navItems = footer?.navItems || []
   return (
     <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
       <Gutter>
@@ -26,7 +31,6 @@ const FooterComponent = () => {
                 height={36}
                 className={classes.Icon}
               />
-
               <h5 className={classes.title}>{inclusion.title}</h5>
               <p>{inclusion.description}</p>
             </li>
@@ -34,7 +38,39 @@ const FooterComponent = () => {
         </ul>
       </Gutter>
 
-      <div className={classes.footer}></div>
+      <div className={classes.footer}>
+        <Gutter>
+          <div className={classes.wrap}>
+            <Link href="/">
+              <Image src="/logo-white.svg" alt="logo" width={170} height={50} />
+            </Link>
+            <p>{footer.copyright}</p>
+            <div className={classes.socialLinks}>
+              {navItems.map(item => {
+                const icon = item?.link?.icon as Media
+
+                return (
+                  <Button
+                    key={item.link.label}
+                    el="link"
+                    href={item.link.url}
+                    newTab={true}
+                    className={classes.socialLinksItem}
+                  >
+                    <Image
+                      src={icon?.url}
+                      alt={item?.link.label}
+                      width={24}
+                      height={24}
+                      className={classes.socialIcon}
+                    />
+                  </Button>
+                )
+              })}
+            </div>
+          </div>
+        </Gutter>
+      </div>
     </footer>
   )
 }
